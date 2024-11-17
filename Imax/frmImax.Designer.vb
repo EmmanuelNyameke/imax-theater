e98bd95aead3d8d5902ce74dacffd03917c8d83a
EmmanuelNyameke <155826955+EmmanuelNyameke@users.noreply.github.com>
2024-11-17 14:55:33 +0000

    Add files via upload

---
+++ Imax/frmImax.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmImax
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        lblDisplayCost = New Label()
+        btnClearForm = New Button()
+        btnTicketCost = New Button()
+        txtTickets = New TextBox()
+        lblTickets = New Label()
+        cboIMAXTickets = New ComboBox()
+        lblHeading = New Label()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(lblDisplayCost)
+        Panel1.Controls.Add(btnClearForm)
+        Panel1.Controls.Add(btnTicketCost)
+        Panel1.Controls.Add(txtTickets)
+        Panel1.Controls.Add(lblTickets)
+        Panel1.Controls.Add(cboIMAXTickets)
+        Panel1.Controls.Add(lblHeading)
+        Panel1.Dock = DockStyle.Right
+        Panel1.Font = New Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Panel1.Location = New Point(436, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(364, 450)
+        Panel1.TabIndex = 0
+        ' 
+        ' lblDisplayCost
+        ' 
+        lblDisplayCost.AutoSize = True
+        lblDisplayCost.BackColor = Color.DodgerBlue
+        lblDisplayCost.ForeColor = Color.White
+        lblDisplayCost.Location = New Point(73, 354)
+        lblDisplayCost.Name = "lblDisplayCost"
+        lblDisplayCost.Size = New Size(73, 23)
+        lblDisplayCost.TabIndex = 6
+        lblDisplayCost.Text = "Label1"
+        lblDisplayCost.Visible = False
+        ' 
+        ' btnClearForm
+        ' 
+        btnClearForm.BackColor = Color.DodgerBlue
+        btnClearForm.ForeColor = Color.White
+        btnClearForm.Location = New Point(102, 284)
+        btnClearForm.Name = "btnClearForm"
+        btnClearForm.Size = New Size(160, 31)
+        btnClearForm.TabIndex = 5
+        btnClearForm.Text = "Clear Form"
+        btnClearForm.UseVisualStyleBackColor = False
+        btnClearForm.Visible = False
+        ' 
+        ' btnTicketCost
+        ' 
+        btnTicketCost.BackColor = Color.DodgerBlue
+        btnTicketCost.ForeColor = Color.White
+        btnTicketCost.Location = New Point(102, 225)
+        btnTicketCost.Name = "btnTicketCost"
+        btnTicketCost.Size = New Size(160, 31)
+        btnTicketCost.TabIndex = 4
+        btnTicketCost.Text = "Ticket Cost"
+        btnTicketCost.UseVisualStyleBackColor = False
+        btnTicketCost.Visible = False
+        ' 
+        ' txtTickets
+        ' 
+        txtTickets.Font = New Font("Verdana", 14.25F, FontStyle.Bold)
+        txtTickets.Location = New Point(161, 148)
+        txtTickets.Name = "txtTickets"
+        txtTickets.Size = New Size(43, 31)
+        txtTickets.TabIndex = 3
+        txtTickets.TextAlign = HorizontalAlignment.Center
+        txtTickets.Visible = False
+        ' 
+        ' lblTickets
+        ' 
+        lblTickets.AutoSize = True
+        lblTickets.Font = New Font("Verdana", 14.25F, FontStyle.Bold)
+        lblTickets.Location = New Point(73, 113)
+        lblTickets.Name = "lblTickets"
+        lblTickets.Size = New Size(242, 23)
+        lblTickets.TabIndex = 2
+        lblTickets.Text = "Number of Ticket(s): "
+        lblTickets.Visible = False
+        ' 
+        ' cboIMAXTickets
+        ' 
+        cboIMAXTickets.Font = New Font("Verdana", 14.25F, FontStyle.Bold)
+        cboIMAXTickets.FormattingEnabled = True
+        cboIMAXTickets.Items.AddRange(New Object() {"Matinee $16", "Evening $27"})
+        cboIMAXTickets.Location = New Point(86, 60)
+        cboIMAXTickets.Name = "cboIMAXTickets"
+        cboIMAXTickets.Size = New Size(193, 31)
+        cboIMAXTickets.TabIndex = 1
+        cboIMAXTickets.Text = "Select Ticket:"
+        ' 
+        ' lblHeading
+        ' 
+        lblHeading.AutoSize = True
+        lblHeading.BackColor = Color.DodgerBlue
+        lblHeading.Font = New Font("Verdana", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblHeading.ForeColor = Color.White
+        lblHeading.Location = New Point(13, 9)
+        lblHeading.Name = "lblHeading"
+        lblHeading.Size = New Size(338, 32)
+        lblHeading.TabIndex = 0
+        lblHeading.Text = "IMAX Theater Tickets"
+        ' 
+        ' frmImax
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.Imax
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(800, 450)
+        Controls.Add(Panel1)
+        Name = "frmImax"
+        Text = "Purchase IMAX Theater Tickets"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblTickets As Label
+    Friend WithEvents cboIMAXTickets As ComboBox
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblDisplayCost As Label
+    Friend WithEvents btnClearForm As Button
+    Friend WithEvents btnTicketCost As Button
+    Friend WithEvents txtTickets As TextBox
+
+End Class
---
+++ Imax/frmImax.vb
@@ -0,0 +1,120 @@
+﻿' Program Name: IMAX Theater Tickets
+' Date: November 11, 2024
+' Author: K Bola
+' Purpose: This application computes the cost of martinee and evening tickets with varying base and data plans
+Public Class frmImax
+    Private Sub cboIMAXTickets_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboIMAXTickets.SelectedIndexChanged
+        Dim intIMAXTickets As Integer
+        intIMAXTickets = cboIMAXTickets.SelectedIndex
+        If (intIMAXTickets = 0 Or intIMAXTickets = 1) Then
+            ' Making items visible
+            lblTickets.Visible = True
+            txtTickets.Visible = True
+            btnTicketCost.Visible = True
+            btnClearForm.Visible = True
+            lblDisplayCost.Visible = True
+        End If
+    End Sub
+
+    'Function Procedure to validate the number of tickets
+    Private Function ValidateNumberOfTickets() As Boolean
+        ' This procedure validates the number of tickets entered
+        Dim intTickets As Integer
+        Dim blnValidityCheck As Boolean = False
+        Dim strNumberOfTicketsMessage As String = "Please enter a valid value."
+        Dim strErrorMessage As String = "Invalid Input."
+        Try
+            intTickets = Convert.ToInt32(txtTickets.Text)
+            If (intTickets > 0 And IsNumeric(intTickets)) Then
+                blnValidityCheck = True
+            Else
+                MsgBox(strNumberOfTicketsMessage, MsgBoxStyle.OkOnly Or MsgBoxStyle.Exclamation, strErrorMessage)
+                txtTickets.Clear()
+                txtTickets.Focus()
+            End If
+        Catch ex As Exception
+            MsgBox(strNumberOfTicketsMessage, MsgBoxStyle.OkOnly Or MsgBoxStyle.Exclamation, strErrorMessage)
+            txtTickets.Clear()
+            txtTickets.Focus()
+        End Try
+        Return blnValidityCheck
+    End Function
+
+    ' Function procedure to validate whether a user selected a ticket type
+    Private Function ValidateTicketsType(ByRef blnTicket As Boolean, ByRef strTicket As String) As Integer
+        ' This function ensures that user selected a ticket type
+        Dim intTicket As Integer
+        Try
+            intTicket = Convert.ToInt32(cboIMAXTickets.SelectedIndex)
+            strTicket = cboIMAXTickets.SelectedItem.ToString()
+            blnTicket = True
+        Catch ex As Exception
+            ' Detecting if a ticket type is not selected
+            MsgBox("Select a ticket type", MsgBoxStyle.OkOnly Or MsgBoxStyle.Exclamation, "Invalid Entry")
+            blnTicket = False
+        End Try
+        Return intTicket
+    End Function
+    ' Function procedures to be called for the tickets to be computed and pass back the cost of the total ticket cost
+    Private Function MartineeCost() As Decimal
+        Const _cdecMartineeCost As Decimal = 16D
+        Dim decTotalCost As Decimal
+        Dim intTicket As Integer = Convert.ToInt32(txtTickets.Text)
+        Dim intTicketType As Integer
+        intTicketType = Convert.ToInt32(cboIMAXTickets.SelectedIndex)
+        If (intTicketType = 0) Then
+            decTotalCost = _cdecMartineeCost * intTicket
+        End If
+        Return decTotalCost
+    End Function
+    Private Function EveningCost() As Decimal
+        Const _cdecEveningCost As Decimal = 27D
+        Dim decTotalCost As Decimal
+        Dim intTickets = Convert.ToInt32(txtTickets.Text)
+        Dim intTicketType As Integer = Convert.ToInt32(cboIMAXTickets.SelectedIndex)
+        If (intTicketType = 1) Then
+            decTotalCost = _cdecEveningCost * intTickets
+        End If
+        Return decTotalCost
+    End Function
+
+    Private Sub btnTicketCost_Click(sender As Object, e As EventArgs) Handles btnTicketCost.Click
+        Dim intTickets As Integer
+        Dim blnNumberOfTickets As Boolean = False
+        Dim blnTicketIsSelected As Boolean = False
+        Dim intTicketChoice As Integer
+        Dim strSelectedTeam As String = ""
+        Dim intIMAXChoice As Integer
+        Dim decTotalCost As Decimal
+        ' Calling a function to ensure the number of tickets entered
+        blnNumberOfTickets = ValidateNumberOfTickets()
+        ' Calling a function to ensure the ticket type was selected
+        intTicketChoice = ValidateTicketsType(blnTicketIsSelected, strSelectedTeam)
+        If (blnNumberOfTickets And blnTicketIsSelected) Then
+            intTickets = Convert.ToInt32(txtTickets.Text)
+            intIMAXChoice = cboIMAXTickets.SelectedIndex
+            Select Case intIMAXChoice
+                Case 0
+                    decTotalCost = MartineeCost()
+                Case 1
+                    decTotalCost = EveningCost()
+            End Select
+            ' Displaying the cost of the ticket
+            lblDisplayCost.Text = decTotalCost.ToString("C") & " for the tickets."
+        End If
+    End Sub
+
+    Private Sub frmImax_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Threading.Thread.Sleep(4000)
+    End Sub
+
+    Private Sub btnClearForm_Click(sender As Object, e As EventArgs) Handles btnClearForm.Click
+        cboIMAXTickets.Text = "Select Ticket:"
+        ' Making items invisible
+        lblTickets.Visible = False
+        txtTickets.Visible = False
+        btnTicketCost.Visible = False
+        btnClearForm.Visible = False
+        lblDisplayCost.Visible = False
+    End Sub
+End Class
